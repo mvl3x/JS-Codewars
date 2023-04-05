99b6a8052fa8e4403798b06c3598366f1e78dcc9
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-04-05 12:07:53 +0500

    feat: add solution to "Student's Final Grade" kata

---
+++ solutions/8-kyu/StudentsFinalGrade.js
@@ -0,0 +1,7 @@
+const finalGrade = (exam, projects) => {
+  return (
+    (exam > 90 || projects > 10) ? 100 :
+    (exam > 75 && projects >= 5) ? 90 :
+    (exam > 50 && projects >= 2) ? 75 : 0
+  );
+}
