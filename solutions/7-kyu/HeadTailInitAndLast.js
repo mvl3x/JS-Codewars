f83aed6c82a351d40581cc70a896079ba3cc3e46
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-04-19 17:20:21 +0500

    refactor: delete test data from HeadTailInitAndLast.js

--- solutions/7-kyu/HeadTailInitAndLast.js
+++ solutions/7-kyu/HeadTailInitAndLast.js
@@ -6,10 +6,3 @@ const tail = (arr) => arr.slice(1);
 const init = (arr) => arr.slice(0, arr.length - 1);
 
 const last = (arr) => arr[arr.length - 1];
-
-const arr = [0, 1, 2, 3, 4, 5];
-
-console.log(head(arr));
-console.log(tail(arr));
-console.log(init(arr));
-console.log(last(arr));
